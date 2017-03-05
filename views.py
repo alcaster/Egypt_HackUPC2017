513b9d305df8ce25509c20c4ebc76fb73f750e0b
okraskaj <okraskaj@ee.pw.edu.pl>
2017-03-05 03:00:46 +0100

    done

--- views.py
+++ views.py
@@ -8,15 +8,18 @@ async_mode = None
 socketio = SocketIO(app, async_mode=async_mode)
 @app.route('/', methods=["GET"])
 def home():
-    return render_template("index.jinja2")
+    return render_template("index_new.jinja2")
 
 @socketio.on('connect', namespace='/test')
 def connect_event():
     print('Client connected', request.sid)
     emit('connected')
+
 @socketio.on('disconnect', namespace='/test')
 def disconnect_event():
     print('Client disconnected', request.sid)
+
+
 @socketio.on('ping', namespace='/test')
 def ping_pong(message):
     print('pong and message={}'.format(message))
